13e72845ff36f02247e58b37d2c0854719d639e2
Jorge314 <157532458+Jorge314@users.noreply.github.com>
2026-06-17 20:37:51 -0600

    Update Pregunta_4.sql

---
+++ scripts/Analysis/Pregunta_4.sql
@@ -0,0 +1,33 @@
+SET search_path TO clima_izta_dwh;
+
+WITH radiacion_continua AS (
+    SELECT 
+        t.fecha_hora,
+        t.anio,
+        t.estacion, -- Nos traemos la estación desde aquí
+        f.radiacion_solar,
+        f.humedad_relativa,
+        -- El promedio móvil ahora mira el pasado real (mayo alimenta a junio)
+        AVG(f.radiacion_solar) OVER (
+            ORDER BY t.fecha_hora 
+            ROWS BETWEEN 23 PRECEDING AND CURRENT ROW
+        ) as radiacion_movil_24h,
+        AVG(f.humedad_relativa) OVER (
+            ORDER BY t.fecha_hora 
+            ROWS BETWEEN 23 PRECEDING AND CURRENT ROW
+        ) as humedad_movil_24h
+    FROM fact_clima f
+    JOIN dim_tiempo t ON f.id_tiempo = t.id_tiempo
+),
+picos_verano AS (
+    SELECT 
+        anio,
+        ROUND(MAX(radiacion_movil_24h), 2) as pico_radiacion_sostenida,
+        ROUND(MIN(humedad_movil_24h), 2) as sequedad_extrema
+    FROM radiacion_continua
+    WHERE estacion = 'Verano' -- El filtro se aplica AQUÍ, con las métricas ya calculadas limpiamente
+    GROUP BY anio
+)
+SELECT * FROM picos_verano
+ORDER BY anio DESC;
+-- ORDER BY sequedad_extrema ASC;
